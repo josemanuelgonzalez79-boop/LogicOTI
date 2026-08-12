d03628fca6266503046908845fa6bcba2e4963f6
Manuel Chavez <josemanuelgonzalez79@gmail.com>
2026-08-11 17:00:59 -0700

    f

--- LogicOTIFront/src/environments/environment.production.ts
+++ LogicOTIFront/src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://192.168.5.66:3210/api',
-  websocketPath: 'ws://192.168.5.66:3210/ws',
+  apiBaseUrl: '/api',
+  websocketPath: '/ws',
 } as const;
